ff476898fbbe0632e3a1afa38112c476c4cdf46f
lvdong <lvdongofficial@qq.com>
2026-09-01 17:22:00 +0800

    fix: 最大化键恒显口形
    - 去最大化态双框图标, 用户预期口形

--- src/client/components/tabs/window-control.jsx
+++ src/client/components/tabs/window-control.jsx
@@ -2,7 +2,7 @@
  * btns
  */
 
-import { CloseOutlined, MinusOutlined, BorderOutlined, CopyOutlined } from '@ant-design/icons'
+import { CloseOutlined, MinusOutlined, BorderOutlined } from '@ant-design/icons'
 import { auto } from 'manate/react'
 import {
   isMacJs
@@ -43,11 +43,10 @@ export default auto(function WindowControl (props) {
           isMaximized ? unmaximize : maximize
         }
       >
-        {
-          isMaximized
-            ? <CopyOutlined title={e('unmaximize')} className='iblock font12 widnow-control-icon' />
-            : <BorderOutlined title={e('maximize')} className='iblock font12 widnow-control-icon' />
-        }
+        <BorderOutlined
+          title={isMaximized ? e('unmaximize') : e('maximize')}
+          className='iblock font12 widnow-control-icon'
+        />
       </div>
       <div className='window-control-box window-control-close' onClick={closeApp}>
         <CloseOutlined title={e('close')} className='iblock font12 widnow-control-icon' />
