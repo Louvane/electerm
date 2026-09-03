ed80d57600ee3f3ac03a66659965bdfc773e75de
lvdong <lvdongofficial@qq.com>
2026-09-03 08:46:56 +0800

    fix: 补回 bookmark-form-drawer notify import

--- src/client/components/anchor/bookmark-form-drawer.jsx
+++ src/client/components/anchor/bookmark-form-drawer.jsx
@@ -7,6 +7,7 @@
 import React, { useState, useEffect } from 'react'
 import { CloseOutlined, PlusOutlined, EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons'
 import { Drawer, Select } from 'antd'
+import { notify } from './anchor-notify'
 import {
   getBookmarks,
   upsertBookmark,
